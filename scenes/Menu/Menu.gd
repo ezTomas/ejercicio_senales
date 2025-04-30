dac6fac4deca0722ad6cff9464f3a49370d2fd26
Enzo Tomas <tomasenzo03@gmail.com>
2025-04-30 14:28:57 -0300

    se conectaron los generadores a la escena "menu"

--- scenes/Menu/Menu.gd
+++ scenes/Menu/Menu.gd
@@ -8,7 +8,10 @@ func _ready():
 	# Inicializa el texto de "CountLabel" usando el texto
 	# Constante "INSTANCES_TEXT" más el valor de "instancesCount" como Texto
 	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
-	pass
+	var red_block = get_node("RedGenerator")
+	red_block.button_down.connect(count_new_instance)
+	var violet_block = get_node("VioletGenerator")
+	violet_block.button_down.connect(count_new_instance)
 
 func count_new_instance():
 	instancesCount += 1

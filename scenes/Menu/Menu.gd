07be7d4c17a1ec713c231806550b73c34f474995
Enzo Tomas <tomasenzo03@gmail.com>
2025-04-30 14:30:27 -0300

    se agrego la funcion de delete en la escena menu

--- scenes/Menu/Menu.gd
+++ scenes/Menu/Menu.gd
@@ -16,3 +16,7 @@ func _ready():
 func count_new_instance():
 	instancesCount += 1
 	$CountLabel.text = str(instancesCount)
+
+func delete():
+	instancesCount = 0
+	$CountLabel.text = str(instancesCount)
